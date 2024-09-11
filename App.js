ee51fc647a940550a5588fcd3dfe55eef126f3ab
MANIPIYUSH <Your GitHub Email>
2024-09-11 06:36:43 +0530

    ep3

--- App.js
+++ App.js
@@ -11,9 +11,23 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "I'm an h2 tag"),
   ]),
 ]);
-
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX 🚀
+  </h1>
+);
 console.log(parent); // object
+const HeadingComponent = () => (
+  <>
+    <div id="container">
+      <Title />
+      <h1 className="heaidng">Namaste React Fucntional Component</h1>
+    </div>
+    <div id="container-2"></div>
+  </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
+root.render(<HeadingComponent />);
